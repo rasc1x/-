caaed892677d3559d896fac4ba905e000bf5b6cc
rasc1x <tasevkosta690@gmail.com>
2026-08-13 12:57:18 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showLove(text) {
+    const modal = document.getElementById('modal');
+    const modalText = document.getElementById('modal-text');
+    modalText.innerText = text;
+    modal.classList.remove('hidden');
+}
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.add('hidden');
+}
